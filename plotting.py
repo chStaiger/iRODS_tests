32f4ad3e117f39badf2ce789eb44b3701c7163c5
chStaiger <christine.staiger@surfsara.nl>
2017-05-31 17:11:27 +0200

    adding two plotting functions for HPC servers and local workstations.

--- plotting.py
+++ plotting.py
@@ -6,6 +6,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.cbook as cbook
 import os
+import numpy
 
 # Read in a list of outputfiles
 def readData(files):
@@ -43,15 +44,27 @@ def readData(files):
         elif c == 'elitebook':
             data = data.replace(c, 'workstation')
 
+    #add new column to calculate gigabit/sec
+    data['size GB'] = data['size']
+
+    #reformat
+    data['size GB'].replace(to_replace="[^0-9]+", value=r"", regex=True, inplace=True)
+    data['size GB'] = pd.to_numeric(data['size GB'])
+
+    idx = data['size GB'] == 10
+    data['size GB'][idx] = 1
+    idx = data['size GB'] == 100
+    data['size GB'][idx] = 0.1
+
+    data['size'].replace('10MB', '100x\n10MB', inplace=True)
 
     return data
-
 
 def plotData(dataFrame):
 
     dfIPUT = dataFrame[dataFrame['iget/iput']=='iput']
     dfIGET = dataFrame[dataFrame['iget/iput']=='iget']
-    
+
     # make single plots for resources
     for resc in dataFrame['iresource'].unique():
         # make single plots for clients
@@ -61,19 +74,98 @@ def plotData(dataFrame):
             data_mean   = dfIPUT[idx].groupby('size').mean()
             data_mean.plot.bar()
             plt.xlabel('size')
-            plt.ylabel('seconds')
+            plt.ylabel('Gbit/s')
+            plt.ylim([0, 9])
             plt.title(resc+' - iput from '+client)
             plt.savefig(resc+'-iput-'+client+'.png')
 
             data_mean   = dfIGET[idx].groupby('size').mean()
             data_mean.plot.bar()
             plt.xlabel('size')
-            plt.ylabel('seconds')
+            plt.ylabel('Gbit/s')
+            plt.ylim([0, 5])
             plt.title(resc+' - iget from '+client)
             plt.savefig(resc+'-iget-'+client+'.png')
 
-files = [f for f in os.listdir("../results/") if f.endswith('.csv')]
-dataFrame = readData(files)
-plotData(dataFrame)
+def plotDataCompute(dataFrame):
+    """
+    Creates separate grouped bar plots for the compute resources and the workstation results.
+    Creates single figures for resource X [iput, iget].
+    Shown is the mean performance per client.
+    """
+    plt.clf()
+
+    dfIPUT = dataFrame[dataFrame['iget/iput']=='iput']
+    dfIGET = dataFrame[dataFrame['iget/iput']=='iget']
+    
+    # make single plots for resources
+    for resc in dataFrame['iresource'].unique():
+        idx = (dataFrame['iresource']==resc)
+        grouped = dfIPUT[idx].groupby(['size', 'client']).agg('mean') # calc of mean and store in column 'real time Gbit/s'
+        groupedPUT = grouped.reset_index().pivot(index='size', columns='client', values='real time Gbit/s')
+
+        grouped = dfIGET[idx].groupby(['size', 'client']).agg('mean')
+        groupedGET = grouped.reset_index().pivot(index='size', columns='client', values='real time Gbit/s')
+
+        # make grouped bar plots for lisa and cartesius
+        groupedPUTComp = groupedPUT[['lisa', 'cartesius']]
+        groupedGETComp = groupedGET[['lisa', 'cartesius']]
+        
+        groupedPUTComp.plot.bar()
+        plt.xlabel('size')
+        plt.ylabel('Gbit/s')
+        plt.title(resc+' - iput')
+        plt.ylim([0, 1])        
+        plt.savefig(resc+'-iput-compute.png')
+        
+        groupedGETComp.plot.bar()
+        plt.xlabel('size')
+        plt.ylabel('Gbit/s')
+
+        plt.savefig(resc+'-iget-compute.png')
+
+def plotDataWorkstation(dataFrame):
+    plt.clf()
+
+    dfIPUT = dataFrame[dataFrame['iget/iput']=='iput']
+    dfIGET = dataFrame[dataFrame['iget/iput']=='iget']
+
+    # make single plots for resources
+    for resc in dataFrame['iresource'].unique():
+        idx = (dataFrame['iresource']==resc)
+        grouped = dfIPUT[idx].groupby(['size', 'client']).agg('mean') # calc of mean and store in column 'real time Gbit/s'
+        groupedPUT = grouped.reset_index().pivot(index='size', columns='client', values='real time Gbit/s')
+
+        grouped = dfIGET[idx].groupby(['size', 'client']).agg('mean')
+        groupedGET = grouped.reset_index().pivot(index='size', columns='client', values='real time Gbit/s')
+
+        # make barplots for workstation
+        groupedPUTWork = groupedPUT['workstation']
+        groupedGETWork = groupedGET['workstation']
+
+        groupedPUTWork.plot.bar()
+        plt.xlabel('size')
+        plt.ylabel('Gbit/s')
+        plt.title(resc+' - iput')
+        plt.ylim([0, 1])
+        plt.savefig(resc+'-iput-workstation.png')
+
+        groupedGETWork.plot.bar()
+        plt.xlabel('size')
+        plt.ylabel('Gbit/s')
+        plt.title(resc+' - iget')
+        plt.ylim([0, 1])
+        plt.savefig(resc+'-iget-workstation.png')
 
 
+def plot():
+    files = [f for f in os.listdir("../results/") if f.endswith('.csv')]
+    dataFrame = readData(files)
+
+    dataFrame['real time Gbit/s'] = dataFrame['size GB']*8/dataFrame['real time']
+    #dataFrame['user time Gbit/s'] = dataFrame['size GB']*8/dataFrame['user time']
+    #dataFrame['system time Gbit/s'] = dataFrame['size GB']*8/dataFrame['system time']
+    
+    plotDataCompute(dataFrame)
+    plotDataWorkstation(dataFrame)
+    
